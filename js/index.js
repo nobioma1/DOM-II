10ed2e7c5d05890de8d9d4bde278f0e46f6d85bc
Noble Obioma <nobioma1@gmail.com>
2019-05-14 13:49:28 +0100

    MouseOver: rotationY on headerImage on mousehover

--- js/index.js
+++ js/index.js
@@ -1,9 +1,18 @@
 // Your code goes here
+// load
 window.addEventListener('load', () => {
-  TweenMax.to('.main-navigation', 1.5, {opacity: 1})
-  TweenMax.to('.intro', 1.5, {opacity: 1, delay: 1.5})
-  TweenMax.to('.content-section', 1.5, {opacity: 1, delay: 3})
-  TweenMax.to('.content-destination', 1.5, {opacity: 1, delay: 4.5})
-  TweenMax.to('.content-pick', 1.5, {opacity: 1, delay: 5.5})
-  TweenMax.to('.footer', 1.5, {opacity: 3, delay: 6.5})
-})
+  TweenMax.to('.main-navigation', 1.5, { opacity: 1 });
+  TweenMax.to('.intro', 1.5, { opacity: 1, delay: 1.5 });
+  TweenMax.to('.content-section', 1.5, { opacity: 1, delay: 3 });
+  TweenMax.to('.content-destination', 1.5, { opacity: 1, delay: 4.5 });
+  TweenMax.to('.content-pick', 1.5, { opacity: 1, delay: 5.5 });
+  TweenMax.to('.footer', 1.5, { opacity: 3, delay: 6.5 });
+});
+
+// mouseover
+const headerImage = document.querySelector('.intro img');
+headerImage.addEventListener('mouseover', () => {
+  TweenMax.from('.intro img', 2, { rotationY: 180, ease: Back.easeOut });
+});
+
+
